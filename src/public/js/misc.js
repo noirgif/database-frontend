8623d1ac177e02c5e1ee5cf91405e140f43c5e43
noirgif <nomaru@outlook.com>
2018-06-03 18:06:54 +0800

    finished insert

--- src/public/js/misc.js
+++ src/public/js/misc.js
@@ -12,6 +12,7 @@ function setTable(tableId, tableHead, tableData) {
     table.appendChild(tableHeadRow);
 
     var tableBody = document.createElement("tbody");
+    tableBody.setAttribute("id", "tablecontents");
     tableData.forEach(function (rowData) {
         var row = document.createElement("tr");
         row.setAttribute("id", rowData.id);
@@ -22,13 +23,14 @@ function setTable(tableId, tableHead, tableData) {
         <button type="button" class="btn btn-default" onclick="delete_row(\'' + rowData.id + '\')">Delete</button>\
         </div>';
 
-        rowData.data.forEach(function (cellData) {
+        rowData.data.forEach(function (cellData, index) {
             var cell = document.createElement("td");
             cell.setAttribute("class", "datacell");
+            cell.setAttribute("name", tableHead[index]);
             var textarea = document.createElement("textarea");
             textarea.setAttribute("class", "form-control datatext");
             textarea.setAttribute("rows", 1);
-            textarea.setAttribute("readonly", "");
+            textarea.setAttribute("readonly", true);
             textarea.appendChild(document.createTextNode(cellData));
             cell.appendChild(textarea);
             row.appendChild(cell);
@@ -36,7 +38,77 @@ function setTable(tableId, tableHead, tableData) {
         row.appendChild(modifying_buttons);
         tableBody.appendChild(row);
     });
+    var inserting_row = document.createElement("tr");
+   // var inserting_cell = document.createElement("td");
+  //  inserting_cell.setAttribute("colspan", tableHead.length + 1);
+
+    var new_row_button = document.createElement("td");
+    new_row_button.setAttribute("colspan", tableHead.length + 1);
+    new_row_button.setAttribute("class", "btn-default");
+    new_row_button.setAttribute("onclick", "insert_row()");
+    new_row_button.appendChild(document.createTextNode("Insert new row"));
+
+ //   inserting_cell.appendChild(new_row_button);
+    inserting_row.appendChild(new_row_button);
+    tableBody.appendChild(inserting_row);
+
     table.appendChild(tableBody);
+}
+
+function insert_row() {
+    console.log("Inserting new row");
+    var tablepath = window.location.pathname.match('/[^/]*')[0];
+    httpGetAsync(tablepath + '/getcolumns', function (e, t) {
+        if (!e) {
+            var columns = JSON.parse(t).filter(column => column !== 'ID');
+            var new_row = document.createElement("tr");
+            new_row.setAttribute("class", "buffered-row");
+            columns.forEach(function (column) {
+                var cell = document.createElement("td");
+                cell.setAttribute("class", "datacell");
+                cell.setAttribute("name", column);
+                var textarea = document.createElement("textarea");
+                textarea.setAttribute("class", "form-control datatext");
+                textarea.setAttribute("rows", 1);
+                cell.appendChild(textarea);
+                new_row.appendChild(cell);
+            });
+            var modifying_buttons = document.createElement("td");
+            modifying_buttons.innerHTML = '<div class="btn-group-vertical">\
+            <button type="button" class="btn btn-default" name="insert">Insert</button>\
+            <button type="button" class="btn btn-default" name="delete">Delete</button>\
+            </div>';
+            new_row.appendChild(modifying_buttons);
+            $('#tablecontents').each(function () {
+                console.log(this);
+                this.insertBefore(new_row, this.lastChild);
+            });
+            $('.buffered-row').find('button[name=insert]').click(function () {
+                $(this).parents('.buffered-row').each(function() {
+                    var data = {};
+                    $(this).children('.datacell').each(function () {
+                        data[$(this).attr('name')] = $(this).children('textarea').val();
+                    });
+                    httpPostAsync(tablepath + '/insert', data, function (e, t) {
+                        if (!e) {
+                            getTableContents();
+                            new_alert('info', t);
+                        }
+                        else {
+                            new_alert('danger', t);
+                        }
+                    });
+                });
+            });
+            $('.buffered-row').find('button[name=delete]').click(function() {
+                $(this).parents('.buffered-row').remove();
+            });
+        }
+        else {
+            console.log(e);
+            new_alert('danger', t);
+        }
+    });
 }
 
 function translate2table(table_object) {
@@ -107,7 +179,7 @@ function getTableContents() {
         if (!e) {
             contents = translate2table(JSON.parse(t));
             setTable("data", contents.head, contents.body);
-            $(".datatext").dblclick(function() {
+            $(".datatext").dblclick(function () {
                 this.removeAttribute('readonly');
             });
         }
@@ -120,8 +192,7 @@ function getTableContents() {
 
 function generateContents() {
     getTableList();
-    if (window.location.pathname == '/')
-    {
+    if (window.location.pathname == '/') {
         // do nothing now
     }
     else
@@ -140,7 +211,7 @@ function update_row(row_id) {
             $('#' + row_id).children('.datacell').each(function (index) {
                 data[columns[index]] = $(this).children('textarea').val();
             });
-            httpPostAsync(tablepath + '/update?id=' + row_id, data, function(e, t) {
+            httpPostAsync(tablepath + '/update?id=' + row_id, data, function (e, t) {
                 if (!e) {
                     getTableContents();
                     new_alert('info', t);
@@ -177,10 +248,10 @@ function new_alert(alert_type, alert_text) {
     alert.setAttribute("class", "alert alert-" + alert_type + " alert-dismissible fade show");
     alert.setAttribute("role", "alert");
     alert.innerHTML = ''
-                    + alert_text
-                    + '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
-                    + '<span aria-hidden="true">&times;</span>'
-                    + '</button>';
+        + alert_text
+        + '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+        + '<span aria-hidden="true">&times;</span>'
+        + '</button>';
     var mainform = document.getElementById('mainform');
     mainform.insertBefore(alert, mainform.firstChild);
 }
